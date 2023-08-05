9e909c6b3c27c3cdf9a8d1951de956415c653406
Turkey9 <141520273+Turkey9@users.noreply.github.com>
2023-08-05 22:14:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_IللD"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("29028317"))
+API_HASH = getenv("8f2831819e4f3fdc44945d2e4d2aa095")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6393692639:AAFVzb7AzkGu5EYVV15SF6kAadIQD0auzFw")
+OWNER_ID = int(getenv("5848055044"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("64cd0227652c571ae5e11041")
 MUST_JOIN = getenv("MUST_JOIN", None)
